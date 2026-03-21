2e4cffb26d7466834084d3081e5efeca88143775
mochinekoo <mochinekoo0113@gmail.com>
2026-03-21 22:14:41 +0900

    [add]　ウインドウが出るようになりました

---
+++ TestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YMM4_Test_Plugin
+{
+    internal class TestViewModel
+    {
+    }
+}
--- main.cs
+++ main.cs
@@ -5,8 +5,12 @@ using System.Windows;
 using YukkuriMovieMaker.Plugin;
 
 namespace YMM4_Test_Plugin {
-    internal class Main : IPlugin {
+    internal class Main : IToolPlugin{
         public string Name => "TestPlugin";
+
+        public Type ViewModelType => typeof(TestViewModel);
+
+        public Type ViewType => typeof(TestWindow);
 
         public Main() {
             MessageBox.Show("あああ", "タイトル", MessageBoxButton.OKCancel, MessageBoxImage.Question);
