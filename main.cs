07a491a93e87998ec24b34ab8ca2f0888d36ac41
mochinekoo <mochinekoo0113@gmail.com>
2026-03-21 21:15:51 +0900

    [add] 起動するとダイアログが出るようになりました

---
+++ main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using YukkuriMovieMaker.Plugin;
+
+namespace YMM4_Test_Plugin {
+    internal class Main : IPlugin {
+        public string Name => "TestPlugin";
+
+        public Main() {
+            MessageBox.Show("あああ", "タイトル", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+        }
+    }
+}
